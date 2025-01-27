7f81588eb9ca339b4154a3a41d3b2a9a0f68286a
Krzysztof Dawidczyk <krzysiek.dawidczyk03@gmail.com>
2025-01-27 11:36:36 +0100

    fix: index displaying current gameweek

--- core/views.py
+++ core/views.py
@@ -1,29 +1,51 @@
 import datetime
 
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from core.models import Player, UpcomingMatch
+from core.models import Player, UpcomingMatch, League
 from accounts.models import CustomUser as CustomUser
+from django.db.models import Min, Max
 
 # Create your views here.
-def index(request):
-    # We will show here upcoming matches for today's date game_week
-    # Check which game_week is today
-    gw = UpcomingMatch.objects.filter(date=datetime.date.today()).first().game_week
-    # Get all matches for that game_week
-    matches = UpcomingMatch.objects.filter(game_week=gw).order_by('date')
 
-    # Group matches by date
+def index(request):
+    # Get today's date
+    today = datetime.date.today()
+
+    # Get leagues names associated with id
+    leagues = League.objects.all()
+    league_dict = {league.id: league.name for league in leagues}
+
+    # Get the distinct game weeks with min and max dates for each league
+    game_weeks = UpcomingMatch.objects.filter(date__lte=today).values('game_week', 'home_team__league').annotate(
+        min_date=Min('date'),
+        max_date=Max('date')
+    ).order_by('game_week', 'home_team__league')
+
+    # Prepare the data for rendering
     grouped_matches = {}
-    for match in matches:
-        match_date = match.date.strftime('%Y-%m-%d')
-        if match_date not in grouped_matches:
-            grouped_matches[match_date] = []
-        grouped_matches[match_date].append(match.get_dictionary())
+    for gw in game_weeks:
+        league_id = gw['home_team__league']
+        league_name = league_dict[league_id]
+        game_week = gw['game_week']
+        min_date = gw['min_date']
+        max_date = gw['max_date']
+        if league_id not in grouped_matches:
+            grouped_matches[league_id] = {
+                'league_name': league_name,
+                'game_weeks': []
+            }
+        grouped_matches[league_id]['game_weeks'].append({
+            'game_week': game_week,
+            'min_date': min_date,
+            'max_date': max_date,
+            'fixtures': UpcomingMatch.objects.filter(
+                home_team__league_id=league_id,
+                game_week=game_week,
+                date__range=(min_date, max_date)
+            ).order_by('date')  # Sortowanie po dacie
+        })
 
     return render(request, 'core/index.html', {
-        'game_week': gw,
         'grouped_matches': grouped_matches
     })
 
